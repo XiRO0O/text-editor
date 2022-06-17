7656f5b26606f78801e30ef5c2c3f6ada8c97ff9
ONKJUNUA <jaseko2002@gmail.com>
2022-06-17 13:16:25 +0200

    save/open functions

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+from pathlib import Path
 
 menu_layout = [
     ['File',['Open','Save','---','Exit']],
@@ -20,7 +21,20 @@ while True:
     if event == sg.WIN_CLOSED:
         break
 
-    if event == "Word Count":
+    if event == 'Open':
+        file_path = sg.popup_get_file('open',no_window = True)
+        if file_path:
+            file = Path(file_path)
+            window['-TEXTBOX-'].update(file.read_text())
+            window['-DOCNAME-'].update(file_path.split('/')[-1])
+
+    if event == 'Save':
+        file_path = sg.popup_get_file('Save as',no_window = True, save_as = True) + '.txt'
+        file = Path(file_path)
+        file.write_text(values['-TEXTBOX-'])
+        window['-DOCNAME-'].update(file_path.split('/')[-1])
+
+    if event == 'Word Count':
         full_text = values['-TEXTBOX-']
         clean_text = full_text.replace('\n',' ').split(' ')
         word_count = len(clean_text)
