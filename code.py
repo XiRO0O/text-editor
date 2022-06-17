59f08f724dd01a049f222fd18df191246d501e2c
ONKJUNUA <jaseko2002@gmail.com>
2022-06-17 13:01:40 +0200

    word count

--- code.py
+++ code.py
@@ -1,12 +1,30 @@
 import PySimpleGUI as sg
 
-layout = [[]]
+menu_layout = [
+    ['File',['Open','Save','---','Exit']],
+    ['Tools',['Word Count']],
+    ['Add']
+]
 
-window = sg.Window("Text editor",layout)
+sg.theme('GrayGrayGray')
+layout = [
+    [sg.Menu(menu_layout)],
+    [sg.Text('Untilted', key = '-DOCNAME-')],
+    [sg.Multiline(no_scrollbar = True, size = (40,30), key = '-TEXTBOX-')]
+]
+
+window = sg.Window('Text editor', layout)
 
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
+
+    if event == "Word Count":
+        full_text = values['-TEXTBOX-']
+        clean_text = full_text.replace('\n',' ').split(' ')
+        word_count = len(clean_text)
+        char_count = len(''.join(clean_text))
+        sg.popup(f'words: {word_count}\ncharacters: {char_count}')
 
 window.close()
